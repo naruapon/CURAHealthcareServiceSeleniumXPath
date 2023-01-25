82fdfda72fd5d8f9a4259b303cdd7e4e1fddaf3b
naruapon <naruapon@webmail.npru.ac.th>
2023-01-25 11:34:21 +0700

    change validate

--- src/Appointment.java
+++ src/Appointment.java
@@ -7,26 +7,28 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
+import junit.framework.Assert;
+
 class Appointment {
 
 	@Test
-	void  TC1001MakeAppointment_Success() throws Exception {
+	void TC1001MakeAppointment_Success() throws Exception {
 		WebDriver driver = null;
 
 		System.setProperty("webdriver.chrome.driver", "D:\\webdriver\\chromedriver.exe");
 		driver = new ChromeDriver();
-		
+
 		driver.get("https://katalon-demo-cura.herokuapp.com/");
-		
-		//Page1 Make Appointment
+
+		// Page1 Make Appointment
 		driver.findElement(By.xpath("//*[@id=\"btn-make-appointment\"]")).click();
-		
-		//Page2 Login
+
+		// Page2 Login
 		driver.findElement(By.xpath("//*[@id=\"txt-username\"]")).sendKeys("John Doe");
 		driver.findElement(By.xpath("//*[@id=\"txt-password\"]")).sendKeys("ThisIsNotAPassword");
 		driver.findElement(By.xpath("//*[@id=\"btn-login\"]")).click();
-		
-		//Page3 Make Appointment Detail
+
+		// Page3 Make Appointment Detail
 		Select option1 = new Select(driver.findElement(By.xpath("//*[@id=\"combo_facility\"]")));
 		option1.selectByVisibleText("Hongkong CURA Healthcare Center");
 		driver.findElement(By.xpath("//*[@id=\"chk_hospotal_readmission\"]")).click();
@@ -35,16 +37,14 @@ class Appointment {
 		driver.findElement(By.xpath("//*[@id=\"txt_visit_date\"]")).sendKeys("18/01/2023");
 		driver.findElement(By.xpath("//*[@id=\"txt_comment\"]")).sendKeys("Heart");
 		driver.findElement(By.xpath("//*[@id=\"btn-book-appointment\"]")).click();
-		
-		String result = driver.findElement(By.id("comment")).getText();
-		
-		if(result.equalsIgnoreCase("Heart")) 
-			System.out.println("Pass");
-		else
-			System.out.println("Fail");
-		
 
+		String result = driver.findElement(By.id("comment")).getText();
+
+		assertEquals("Heart", result);
 		
+		String title = driver.getTitle();
+		System.out.println(title);
+
 		Thread.sleep(5000);
 		driver.quit();
 	}
